e134c8894ad79177c5f7aceedd27d18a540312ff
aaronzomback <aaronzomback@gmail.com>
2022-09-09 01:14:22 +0200

    init

---
+++ src/main/java/com/timesink/scale/TimesinkApplication.java
@@ -0,0 +1,13 @@
+package com.timesink.scale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timesink/scale/TimesinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timesink.scale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
